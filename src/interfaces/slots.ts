f00ae5558514f13d0910a9058a4823f86ae96554
Karol Moroz <k.j.moroz@gmail.com>
2021-10-30 02:24:21 +0200

    Stub querying slots and displaying them in a table

---
+++ src/interfaces/slots.ts
@@ -0,0 +1,9 @@
+import { ID } from "./common";
+
+export interface Slot {
+  id: ID;
+  startTime: number;
+  endTime: number;
+  createdAt: number;
+  updatedAt: number;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
 import { Link } from "react-router-dom";
+import { useListSlotsQuery } from "../api/queries/slots";
+import { formatTimestamp } from "../helpers/TimeHelpers";
 
 const Home = () => {
+  const { data, loading } = useListSlotsQuery();
   return (
     <div>
       <h1>Home</h1>
+      {data ? (
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Start time</th>
+            <th>End time</th>
+          </thead>
+          <tbody>
+            {data.slots.map((row) => (
+              <tr key={row.id}>
+                <td>{row.id}</td>
+                <td>{formatTimestamp(row.startTime)}</td>
+                <td>{formatTimestamp(row.endTime)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
       <Link to="/form">Form</Link>
     </div>
   );
